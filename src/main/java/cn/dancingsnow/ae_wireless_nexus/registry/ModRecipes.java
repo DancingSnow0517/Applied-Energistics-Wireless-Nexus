b58d06bb3fa3d053d77dc49537a80443fe3d6e7e
DancingSnow <1121149616@qq.com>
2026-07-19 17:43:13 +0800

    recipe

--- src/main/java/cn/dancingsnow/ae_wireless_nexus/registry/ModRecipes.java
+++ src/main/java/cn/dancingsnow/ae_wireless_nexus/registry/ModRecipes.java
@@ -1,18 +1,19 @@
 package cn.dancingsnow.ae_wireless_nexus.registry;
 
-import appeng.api.AEApi;
-import gregtech.api.enums.TierEU;
-import gregtech.api.util.GTRecipeBuilder;
-import gregtech.api.util.recipe.Scanning;
-import gtPlusPlus.core.material.MaterialsAlloy;
-import net.minecraft.item.ItemStack;
-
 import static gregtech.api.util.GTRecipeBuilder.INGOTS;
 import static gregtech.api.util.GTRecipeBuilder.MINUTES;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 import static gregtech.api.util.GTRecipeConstants.AssemblyLine;
 import static gregtech.api.util.GTRecipeConstants.RESEARCH_ITEM;
 import static gregtech.api.util.GTRecipeConstants.SCANNING;
+
+import net.minecraft.item.ItemStack;
+
+import appeng.api.AEApi;
+import gregtech.api.enums.TierEU;
+import gregtech.api.util.GTRecipeBuilder;
+import gregtech.api.util.recipe.Scanning;
+import gtPlusPlus.core.material.MaterialsAlloy;
 
 /**
  * Recipe registration is intentionally left empty until the item costs are confirmed.
@@ -23,10 +24,30 @@ public final class ModRecipes {
     private ModRecipes() {}
 
     public static void postInit() {
-        ItemStack controller = AEApi.instance().definitions().blocks().controller().maybeStack(1).orNull();
-        ItemStack wirelessHub = AEApi.instance().definitions().blocks().wirelessHub().maybeStack(1).orNull();
-        ItemStack wirelessTool = AEApi.instance().definitions().items().toolWirelessKit().maybeStack(1).orNull();
-        ItemStack wireless = AEApi.instance().definitions().materials().wireless().maybeStack(4).orNull();
+        ItemStack controller = AEApi.instance()
+            .definitions()
+            .blocks()
+            .controller()
+            .maybeStack(1)
+            .orNull();
+        ItemStack wirelessHub = AEApi.instance()
+            .definitions()
+            .blocks()
+            .wirelessHub()
+            .maybeStack(1)
+            .orNull();
+        ItemStack wirelessTool = AEApi.instance()
+            .definitions()
+            .items()
+            .toolWirelessKit()
+            .maybeStack(1)
+            .orNull();
+        ItemStack wireless = AEApi.instance()
+            .definitions()
+            .materials()
+            .wireless()
+            .maybeStack(4)
+            .orNull();
 
         if (controller == null || wirelessHub == null || wirelessTool == null || wireless == null) {
             return;
